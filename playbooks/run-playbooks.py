b5f4cba6a4e154a4ce8153c8c7ed39a283a3791c
Veeresh Aradhya <varadhya@redhat.com>
2020-06-22 13:58:03 +0530

    printing output

--- playbooks/run-playbooks.py
+++ playbooks/run-playbooks.py
@@ -117,6 +117,7 @@ time.sleep(120)
 print('******************************** Install tkn on machine {} *************************************'.format(vm_name), flush=True)
 output = subprocess.run('ansible-playbook install-tkn-{}.yml -v -i hosts --private-key id_rsa'.format(os_type).split(), stdout=subprocess.PIPE, text=True)
 print('installation tkn was successful', flush=True)
+print(output.stdout)
 
 if output.returncode == 0:
 	print('******************************** running cli tests on  {} *************************************'.format(vm_name), flush=True)
