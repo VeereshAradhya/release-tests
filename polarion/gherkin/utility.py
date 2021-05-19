1654de57323e167792119d0f963a90c44138fc3c
Veeresh Aradhya <varadhya@redhat.com>
2021-05-19 14:55:30 +0530

    fixing print issue

--- polarion/gherkin/utility.py
+++ polarion/gherkin/utility.py
@@ -4,10 +4,10 @@ from pylarion.exceptions import PylarionLibException
 
 def create_or_update_test(project_id, test_id, test_properties):
     try:
-        t = TestCase(project_id=project_id, work_item_id=test_id, flush=True)
+        t = TestCase(project_id=project_id, work_item_id=test_id)
         for item in test_properties:
             setattr(t, item, test_properties[item])
-        print('Updating test case {}'.format(test_id))
+        print('Updating test case {}'.format(test_id), flush=True)
         t.update()
     except PylarionLibException:
         t = TestCase(project_id)
