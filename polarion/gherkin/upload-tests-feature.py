b86efdd8bdb07081f940cc66feef7ec7ccaea597
Veeresh Aradhya <varadhya@redhat.com>
2021-05-19 14:35:34 +0530

    adding script to upload gherkin based tests

--- polarion/gherkin/upload-tests-feature.py
+++ polarion/gherkin/upload-tests-feature.py
@@ -3,21 +3,70 @@ from gherkin.parser import Parser
 import re
 from prettytable import PrettyTable
 from utility import create_or_update_test
+import yaml
+import argparse
 
-token_scanner = TokenScanner(
-    '/home/varadhya/workspace/src/github.com/console/frontend/packages/pipelines-plugin/integration-tests/features/pipelines/create-from-add-options.feature')
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--file_path', type=str, help='path to the folder which contains test files',
+                    required=True)
+args = parser.parse_args()
+file_path = args.file_path
+
+# get configs
+with open('config.yaml') as f:
+    try:
+        config = yaml.safe_load(f)
+    except yaml.YAMLError as e:
+        print(e, flush=True)
+        raise Exception
+
+# set project_id and test_id
+project_id = config.get('project_id')
+if not project_id:
+    raise Exception('Please add file project_id in config.yaml')
+
+testcase_regex = config.get('testcase_regex')
+if not testcase_regex:
+    raise Exception('Please add field testcase_regex in config.yaml with valid regex')
+else:
+    try:
+        compiled_testcase_id = re.compile(r'{}'.format(testcase_regex))
+    except re.error as e:
+        print('Invalid value for testcase_regex in config.yaml', flush=True)
+        raise Exception
+
+token_scanner = TokenScanner(file_path)
+'/home/varadhya/test-ui/console/frontend/packages/pipelines-plugin/integration-tests/features/pipelines/create-from-add-options.feature'
 parser = Parser()
 content = parser.parse(token_scanner)
 # print(len(content['feature']['children'][2]['scenario']['examples'][0]['tableBody']))
 
 for children in content['feature']['children'][1:]:
     tc_title = children['scenario']['name']
-    tc_id = re.search(r'P-\d+-TC\d+', tc_title)
-    tc_id = tc_id.group(0)
+    tc_id = compiled_testcase_id.match(tc_title)
+    tc_id = tc_id.groups()[0]
     if tc_id:
         test_properties = {}
+        test_properties['tags'] = ','.join(map(lambda x: x['name'], children['scenario']['tags']))
+        if '@to-do' in test_properties['tags']:
+            test_properties['caseautomation'] = 'notautomated'
+        elif '@manual' in test_properties['tags']:
+            test_properties['caseautomation'] = 'manualonly'
+        else:
+            test_properties['caseautomation'] = 'automated'
+
+        # set defaults
+        defaults = config.get('defaults')
+        if defaults:
+            # print('inside defaults')
+            for default in defaults:
+                test_properties[default['field_name_api']] = default['value']
+                # print(test_properties[default['field_name_api']])
         tc_title = re.sub(r':.+', '', tc_title).strip()
-        tc_description = map(lambda x: '<li>{}{}</li>'.format(x['keyword'], x['text']), children['scenario']['steps'])
+        tc_description = map(
+            lambda x: '<li>{}{}</li>'.format(x['keyword'], (x['text'].replace('<', '&lt;')).replace('>', '&gt;')),
+            children['scenario']['steps'])
         tc_description = '<ul>\n ' + '\n '.join(tc_description) + '\n</ul>'
         if children['scenario']['examples']:
             i = 1
@@ -26,29 +75,14 @@ for children in content['feature']['children'][1:]:
                     table = PrettyTable()
                     table.field_names = map(lambda x: x['value'], example['tableHeader']['cells'])
                     table.add_row(list(map(lambda x: x['value'], table_body['cells'])))
-                    # table.field_names = heading/
-                    # table.add_row(list(values))
-                    # print(tc_title, '{}-{}'.format(tc_id, ('{}'.format(i)).zfill(2)))
-                    # print(tc_description)
-                    # # print(list(header))
-                    # # print(list(l))
-                    # print(table)
-                    # print('\n')
-                    # i = i+1
                     new_tc_id = '{}-{}'.format(tc_id, ('{}'.format(i)).zfill(2))
                     table = '<br/>\n'.join(table.get_string().split('\n'))
-                    tc_description.replace('<', '&lt;')
-                    tc_description.replace('>', '&gt;')
                     test_properties['title'] = tc_title
                     test_properties['desc'] = '{}<br/>\n{}'.format(tc_description, table)
                     test_properties['description'] = test_properties['desc']
-                    create_or_update_test('VAradhyaExerciseAug30', new_tc_id, test_properties)
-                    i = i+1
+                    create_or_update_test(project_id, new_tc_id, test_properties)
+                    i = i + 1
         else:
-            # print(tc_title, tc_id)
-            # print(tc_description)
-            # print('\n')
-
             test_properties['title'] = tc_title
             test_properties['desc'] = tc_description
             test_properties['description'] = tc_description
